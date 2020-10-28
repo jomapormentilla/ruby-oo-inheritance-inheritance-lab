1930e99406166c0091104e3f8ffb2f42ab9b5592
jomapormentilla <jomapormentilla@gmail.com>
2020-10-28 13:36:54 -0400

    commit tree 981177d41d1b39493dbf5270d45ac2ea35f37a9c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+
+    @@all = []
+
+    def initialize
+        @knowledge = []
+        @@all << self
+    end
+
+    def learn( knowledge )
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def self.all
+        @@all
+    end
 end
